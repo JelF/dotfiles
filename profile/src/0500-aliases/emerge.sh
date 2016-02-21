d3ee94955e82333413bc9c937cdfaa045e4ba848
Alexander Smirnov <begdory4@gmail.com>
2016-02-21 19:01:29 +0300

    some nice aliases

---
+++ profile/src/0500-aliases/emerge.sh
@@ -0,0 +1,2 @@
+# USE ON: bash zsh
+alias emerge1="sudo ionice -n+1 nice -n+1 emerge"
---
+++ profile/src/0500-aliases/nice.sh
@@ -0,0 +1,5 @@
+# USE ON: zsh bash 
+
+alias in1="ionice -n+1"
+alias n1="nice -n+1"
+alias smart="ionice -n+1 nice -n+1"
